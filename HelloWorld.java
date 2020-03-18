99247e128b1e855011bf53674c7bc83498804434
KANGKYEONGSEOK <62289946+KANGKYEONGSEOK@users.noreply.github.com>
2020-03-18 09:24:16 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package hellowworld;
+
+public class Hellowworld {
+	public static void main(String[] args) {
+       System.out.println("hellow, world!");
+       System.out.println("i am a new java programer");
+	}
+}
